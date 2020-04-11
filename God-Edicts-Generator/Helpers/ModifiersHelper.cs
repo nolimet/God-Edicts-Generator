6fc365cd30f75dd9569c7187cd1069b47570ea79
Jesse Stam <nolimet@stamdata.nl>
2020-04-11 22:58:41 +0200

    added helper for Modifiers

---
+++ God-Edicts-Generator/Helpers/ModifiersHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GodEdictGen.Data;
+
+namespace GodEdictGen.Helpers
+{
+    public static class ModifiersHelper
+    {
+        public static IReadOnlyList<ModifierGenerator> AddSet(this IReadOnlyList<ModifierGenerator> orignalList, string modifierFormat, double modifierValue, params string[] modifierNames)
+        {
+            return orignalList.Concat(ModifierGenerator.GenerateSet(modifierFormat, modifierValue, modifierNames)).ToArray();
+        }
+    }
+}
