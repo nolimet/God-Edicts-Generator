8cc6e522c5402c33f1aff5c9be644f1d95517a07
Jesse Stam <nolimet@stamdata.nl>
2020-04-11 23:50:07 +0200

    Updated assembly Info

--- God-Edicts-Generator/Properties/AssemblyInfo.cs
+++ God-Edicts-Generator/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TextGen")]
+[assembly: AssemblyTitle("GodEdictGen")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TextGen")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("Jesse D. Stam")]
+[assembly: AssemblyProduct("GodEdictGen")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
