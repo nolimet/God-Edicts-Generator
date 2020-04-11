af2672ef37d68c6633ccca23d35958963f3bb171
Jesse Stam <nolimet@stamdata.nl>
2020-04-12 01:52:58 +0200

    added icon to all edict names

--- God-Edicts-Generator/Data/LangueGenerator.cs
+++ God-Edicts-Generator/Data/LangueGenerator.cs
@@ -15,9 +15,9 @@ namespace GodEdictGen.Data
         {
             return
             $"#godEdict_{edict.name}\n" +
-            $"godEdict_{edict.name}:0 \"£god_Edict_Icon {edict.nicename} \"\n" +
-            $"edict_godEdict_{edict.name}_on:0 \"£trigger_no Enable {edict.nicename} \"\n" +
-            $"edict_godEdict_{edict.name}_off:0 \"£trigger_yes Disable {edict.nicename} \"\n" +
+            $"godEdict_{edict.name}:0 \"£god_Edict_Icon£ {edict.nicename} \"\n" +
+            $"edict_godEdict_{edict.name}_on:0 \"£god_Edict_Icon£ £trigger_no Enable {edict.nicename} \"\n" +
+            $"edict_godEdict_{edict.name}_off:0 \"£god_Edict_Icon£ £trigger_yes Disable {edict.nicename} \"\n" +
             "\n" +
             $"edict_godEdict_{edict.name}_on_desc:0 \"Enable {edict.nicename} modifier \"\n" +
             $"edict_godEdict_{edict.name}_off_desc:0 \"Disable {edict.nicename} modifier \"\n\n";
